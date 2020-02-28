383490c9dddec9c51555953233e42d578413af47
snehkat <snehavrushabk@gmail.com>
2020-02-28 14:16:05 +0530

    final commit

---
+++ src/app/common/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { LogstatusService } from 'src/app/services/logstatus.service';
+import { Router } from '@angular/router'
+
+interface tokenstring{
+  access_token:string
+}
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+  //providers:[LogstatusService]
+})
+export class LoginComponent implements OnInit {
+logStatus:boolean
+styleObj={margin:"150px",padding:"50px",height:"300px",width:"400px",
+border:"3px solid darked",boxShadow:"5px 5px 10px black"}
+username:string
+password:string
+  constructor(private ls:LogstatusService ,private router:Router) {
+    this.logStatus=this.ls.getStatus()
+   }
+
+   logUser(){
+
+    let data="username="+this.username+"&password="+this.password+"&grant_type=password";
+    this.ls.login(data).subscribe(
+      (data:tokenstring)=>{
+          localStorage.setItem("token",data.access_token)
+          localStorage.setItem("username",this.username)
+          this.router.navigate(['/'])
+      },
+      ()=>{
+        alert("Unable to login")
+      }
+    )
+   }
+
+  ngOnInit(): void {
+  }
+
+}
